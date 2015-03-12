e511ffdb816e8d41ca8b002b400b3068d7005226
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-03-11 22:39:46 -0400

    Added some ideas to project as comments

--- src/nyc/c4q/AnthonyFermin/Main.java
+++ src/nyc/c4q/AnthonyFermin/Main.java
@@ -3,6 +3,20 @@ package nyc.c4q.AnthonyFermin;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //Welcome to the Gauntlet!
+        //Different mini games that's randomized, if you win 3 out of 5 then you Win the whole game!!!
+        //TIC TAC TOE
+
+        System.out.println(" 1 | 2 | 3 ");
+        System.out.println(" ---------");
+        System.out.println(" 4 | 5 | 6 ");
+        System.out.println(" ---------");
+        System.out.println(" 7 | 8 | 9 ");
+
+        //HOP (with wind and direction)
+
+        //last game of blackJack
+
+        //
     }
 }
